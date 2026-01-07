9f52fd5069cde702876d2beb440f7bfebcd3c951
yang107-Xiao <azalebutler2369@gmail.com>
2026-01-08 01:06:59 +0800

    Delete lite-quiz-project directory

--- lite-quiz-project/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  base: '/lite-quiz/', // 这里对应你的 GitHub 仓库名
-})
